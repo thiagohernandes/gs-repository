6f455a0a959caaa3cabc47ac42eb6d92cc05c6bd
thiagohernandes <hernandes_sp@yahoo.com.br>
2019-02-18 11:07:39 -0300

    Criação do projeto

---
+++ gs-crawler/src/main/java/com/br/gs/gscrawler/GsCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.br.gs.gscrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsCrawlerApplication.class, args);
+	}
+
+}
